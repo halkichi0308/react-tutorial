08cc3baddfabab84a5420f2e2d9416cab1ce63d9
halkichi0308 <m.kouda0308@gmail.com>
2021-04-29 15:43:31 +0900

    Replace function Component.

--- src/index.js
+++ src/index.js
@@ -4,51 +4,45 @@ import './index.css';
 
 function Square(props){
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} >
       {props.value}
     </button> 
   )
 }
 
-class Board extends React.Component {
-  constructor(props){
-    super(props)
-  }
-  //constructorがあってもokなはず
-  renderSquare(i) {
+function Board(props){
+  const renderSquare = (number) =>{
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={()=> this.props.onClick(i)}
+        value={props.squares[number]}
+        onClick={()=> props.onClick(number)}
       />
       )
   }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+
+
 }
 
-//https://ja.reactjs.org/tutorial/tutorial.html#lifting-state-up-again
-// こっから!
+//親Component
 class Game extends React.Component {
   constructor(props){
     super(props)
